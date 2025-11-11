420bbbda6f8e05fd91e6c1ff14bb482ec51bb9e4
unicorn <dds04031@gmail.com>
2025-11-11 21:51:24 +0700

    Start First Time

---
+++ src/main/java/com/duha/duhabank/DuhabankApplication.java
@@ -0,0 +1,13 @@
+package com.duha.duhabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuhabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuhabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duha/duhabank/DuhabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duha.duhabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuhabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
